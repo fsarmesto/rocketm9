006603396c77f77f2c39178ddd2804c132fb3281
fsarmesto <puru.infotech@gmail.com>
2025-02-06 17:45:05 +0100

    Added star-fall
    Modified .js, added a file and an import for animations (to split the css) and added some styles for the class "stars" (also made it more clear what it is by adding section before it.

--- scripts/script.js
+++ scripts/script.js
@@ -6,13 +6,21 @@ function f_main(){
     const VH = window.innerHeight;
     const VW = window.innerWidth;
 
-
-    for(let i = 0; i < 12; i++){
+    for(let i = 0; i < 20; i++){
         let randNumb = (Math.floor(Math.random() * VW))-20;
         let star = document.createElement('div');
-        star.style.margin = randNumb;
         star.classList.add('star');
-        console.info(star);
+        star.innerText = "★";
+        star.setAttribute('style','margin-left:'+randNumb+"px");
         container.append(star);
     }
+
+    let copy = container.cloneNode(true);
+    setTimeout(() => {
+        container.before(copy);
+    },4000);
+    let copy2 = container.cloneNode(true);
+    setTimeout(() => {
+        copy.before(copy2);
+    },6000);
 }
